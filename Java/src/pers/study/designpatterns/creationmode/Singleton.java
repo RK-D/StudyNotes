6f5cd2f38989f059225ac00bae688b3afa269a74
RK-D <3245885483@qq.com>
2020-05-23 10:28:49 +0800

    only for study

---
+++ Java/src/pers/study/designpatterns/creationmode/Singleton.java
@@ -0,0 +1,23 @@
+package pers.study.designpatterns.creationmode;
+
+/**
+ * @author rookie
+ * @version 1.0
+ * @date 2020/5/23 9:51
+ */
+public class Singleton {
+    //创建 Singleton 的一个对象
+    private static Singleton instance = new Singleton();
+    //让构造函数为 private，这样该类就不会被实例化
+    private Singleton(){}
+    //获取唯一可用的对象
+    public static Singleton getInstance(){
+        return instance;
+    }
+
+    public void showMessage(){
+        System.out.println("单例1");
+    }
+}
+
+
---
+++ Java/src/pers/study/designpatterns/creationmode/SingletonLazy.java
@@ -0,0 +1,33 @@
+package pers.study.designpatterns.creationmode;
+
+/**
+ * @author rookie
+ * @version 1.0
+ * @date 2020/5/23 10:06
+ */
+/**
+ * 1、懒汉式，线程不安全
+ 是否 Lazy 初始化：是
+ 是否多线程安全：否
+ 实现难度：易
+ 描述：这种方式是最基本的实现方式，这种实现最大的问题就是不支持多线程。因为没有加锁 synchronized，所以严格意义上它并不算单例模式。
+ 这种方式 lazy loading 很明显，不要求线程安全，在多线程不能正常工作。
+ */
+public class SingletonLazy {
+
+    private static SingletonLazy instance;
+    private SingletonLazy (){}
+    // 2.线程安全版本
+    //public static synchronized SingletonLazy getInstance()
+
+    //优点：第一次调用才初始化，避免内存浪费。
+    //缺点：必须加锁 synchronized 才能保证单例，但加锁会影响效率。
+    //getInstance() 的性能对应用程序不是很关键（该方法使用不太频繁）。
+    public static SingletonLazy getInstance() {
+        if (instance == null) {
+            instance = new SingletonLazy();
+        }
+        return instance;
+    }
+
+}
